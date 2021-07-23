5610851465e16e7870d4f65dc9ae5cb0fd77d425
Andrew Jonson <andrew_jonson09@yahoo.com>
2021-07-23 12:23:03 +0800

    Added error trap on modular trait

--- app/Traits/ModularTrait.php
+++ app/Traits/ModularTrait.php
@@ -2,6 +2,7 @@
 namespace App\Traits;
 
 use App\Scopes\ModularScope;
+use Illuminate\Auth\AuthenticationException;
 use App\Services\ApiService\v1\PaisTemplateService;
 
 trait ModularTrait
@@ -10,17 +11,21 @@ trait ModularTrait
     {
         static::addGlobalScope(new ModularScope);
         $service = new PaisTemplateService;
-        $userId = $service->currentUser()['data']['id'];
-        static::creating(function ($model) use($userId) {
-            $model->created_by = $userId;
-        });
+        try {
+            $userId = $service->currentUser()['data']['id'];
+            static::creating(function ($model) use($userId) {
+                $model->created_by = $userId;
+            });
 
-        static::updating(function ($model) use($userId) {
-            $model->updated_by = $userId;
-        });
+            static::updating(function ($model) use($userId) {
+                $model->updated_by = $userId;
+            });
 
-        static::deleting(function ($model) use($userId) {
-            $model->deleted_by = $userId;
-        });
+            static::deleting(function ($model) use($userId) {
+                $model->deleted_by = $userId;
+            });
+        } catch (\Exception $e) {
+            throw new AuthenticationException;
+        }
     }
 }
