14f05c28fb67ffcc9394fe8186381502f57f9595
Andrew Jonson <andrew_jonson09@yahoo.com>
2021-09-18 00:12:42 +0800

    Fixed bug in unauthenticated

--- app/Traits/ModularTrait.php
+++ app/Traits/ModularTrait.php
@@ -12,34 +12,30 @@ trait ModularTrait
     {
         static::addGlobalScope(new ModularScope);
         if (!app()->runningInConsole()) {
-            try {
-                $token = request()->bearerToken();
-                if (!Cache::has('current-user-'.$token)) {
-                    $service = new PaisTemplateService;
-                    $user = $service->currentUser();
-                    Cache::set('current-user-'.$token, $user, 300);
-                }
-                
-                $user = Cache::get('current-user-'.$token);
-                $teamId = $user['data']['team']['id'];
-                $userId = $user['data']['id'];
-                $userId = hashid_decode($userId);
-                $teamId = hashid_decode($teamId);
-                static::creating(function ($model) use($userId, $teamId) {
-                    $model->team_id = $teamId;
-                    $model->created_by = $userId;
-                });
-
-                static::updating(function ($model) use($userId) {
-                    $model->updated_by = $userId;
-                });
-
-                static::deleting(function ($model) use($userId) {
-                    $model->deleted_by = $userId;
-                });
-            } catch (\Exception $e) {
-                throw new AuthenticationException;
+            $token = request()->bearerToken();
+            if (!Cache::has('current-user-'.$token)) {
+                $service = new PaisTemplateService;
+                $user = $service->currentUser();
+                Cache::set('current-user-'.$token, $user, 300);
             }
+            
+            $user = Cache::get('current-user-'.$token);
+            $teamId = $user['data']['team']['id'];
+            $userId = $user['data']['id'];
+            $userId = hashid_decode($userId);
+            $teamId = hashid_decode($teamId);
+            static::creating(function ($model) use($userId, $teamId) {
+                $model->team_id = $teamId;
+                $model->created_by = $userId;
+            });
+
+            static::updating(function ($model) use($userId) {
+                $model->updated_by = $userId;
+            });
+
+            static::deleting(function ($model) use($userId) {
+                $model->deleted_by = $userId;
+            });
         }
     }
 }
