62b22815b1c19a7e73d45ba1092f1a681427f32d
Andrew Jonson <andrew_jonson09@yahoo.com>
2021-10-05 14:03:33 +0800

    Modifed ModularTrait

--- app/Traits/ModularTrait.php
+++ app/Traits/ModularTrait.php
@@ -2,40 +2,49 @@
 namespace App\Traits;
 
 use App\Scopes\ModularScope;
+use App\Traits\ResponseTrait;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Auth\AuthenticationException;
 use App\Services\ApiService\v1\PaisTemplateService;
 
 trait ModularTrait
 {
+    use ResponseTrait;
+    
     protected static function booted()
     {
         static::addGlobalScope(new ModularScope);
         if (!app()->runningInConsole()) {
-            $token = request()->bearerToken();
-            if (!Cache::has('current-user-'.$token)) {
-                $service = new PaisTemplateService;
-                $user = $service->currentUser();
-                Cache::set('current-user-'.$token, $user, 300);
+            try {
+                $token = request()->bearerToken();
+                if ($token) {
+                    if (!Cache::has('current-user-'.$token)) {
+                        $service = new PaisTemplateService;
+                        $user = $service->currentUser();
+                        Cache::set('current-user-'.$token, $user, 500);
+                    } else {
+                        $user = Cache::get('current-user-'.$token);
+                    }
+                    
+                    $teamId = $user['data']['team']['id'];
+                    $userId = $user['data']['id'];
+                    $userId = hashid_decode($userId);
+                    $teamId = hashid_decode($teamId);
+                    static::creating(function ($model) use($userId, $teamId) {
+                        $model->team_id = $teamId;
+                        $model->created_by = $userId;
+                    });
+        
+                    static::updating(function ($model) use($userId) {
+                        $model->updated_by = $userId;
+                    });
+        
+                    static::deleting(function ($model) use($userId) {
+                        $model->deleted_by = $userId;
+                    });
+                }
+            } catch(\Exception $e) {
+                return $this->failedResponse($e->getMessage(), 500);
             }
-            
-            $user = Cache::get('current-user-'.$token);
-            $teamId = $user['data']['team']['id'];
-            $userId = $user['data']['id'];
-            $userId = hashid_decode($userId);
-            $teamId = hashid_decode($teamId);
-            static::creating(function ($model) use($userId, $teamId) {
-                $model->team_id = $teamId;
-                $model->created_by = $userId;
-            });
-
-            static::updating(function ($model) use($userId) {
-                $model->updated_by = $userId;
-            });
-
-            static::deleting(function ($model) use($userId) {
-                $model->deleted_by = $userId;
-            });
         }
     }
 }
