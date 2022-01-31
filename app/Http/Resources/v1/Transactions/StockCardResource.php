cbc0c9184f3f8b8068f4a676dd36237b2b7f6080
Diana <dianamagno20@gmail.com>
2022-01-31 09:12:38 +0800

    update response of RIS

--- app/Http/Resources/v1/Transactions/StockCardResource.php
+++ app/Http/Resources/v1/Transactions/StockCardResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\v1\Transactions;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\v1\Transactions\InventoryResource;
+use App\Http\Resources\v1\Transactions\StockCardReferenceResource;
 
 class StockCardResource extends JsonResource
 {
@@ -19,6 +20,7 @@ class StockCardResource extends JsonResource
             'id' => hashid_encode($this->id),
             'stock_card_nr' => $this->stock_card_nr,
             'inventory' => $this->inventory ? new InventoryResource($this->inventory) : null,
+            'references' => StockCardReferenceResource::collection($this->stockCardReference),
             'entity_name' => $this->inventory->iar->entity_name,
             'fund_cluster' => $this->inventory->iar->fundCluster->name,
             'description' => $this->inventory->description
